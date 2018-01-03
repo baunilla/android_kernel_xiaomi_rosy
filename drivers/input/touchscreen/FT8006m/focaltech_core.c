172b7d49dcc257b13fa934c497f722f8d1c0b6e8
Venkata Prahlad Valluru <vvalluru@codeaurora.org>
2018-01-03 16:43:31 +0530

    focaltech: Handle suspend event in focaltech driver
    Add VSYNC_SUSPEND as a touch suspend event.
    This event is generated when display is dimmed.
    Touch can be suspended on this event.
    
    Change-Id: I548dd2efc2a4545cffa98bc1f20549b23cb558bf
    Signed-off-by: Venkata Prahlad Valluru <vvalluru@codeaurora.org>

--- drivers/input/touchscreen/FT8006m/focaltech_core.c
+++ drivers/input/touchscreen/FT8006m/focaltech_core.c
@@ -1064,7 +1064,7 @@ static int fb_notifier_callback(struct notifier_block *self,
         blank = evdata->data;
         if (*blank == FB_BLANK_UNBLANK)
             ft8006m_ts_resume(&fts_data->client->dev);
-        else if (*blank == FB_BLANK_POWERDOWN)
+        else if (*blank == FB_BLANK_POWERDOWN || *blank == FB_BLANK_VSYNC_SUSPEND)
             ft8006m_ts_suspend(&fts_data->client->dev);
     }
 
